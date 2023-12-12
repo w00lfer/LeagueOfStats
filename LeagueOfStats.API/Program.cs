f6de91c392654790196515f84b91bcddf7b9a3e7
Arek <arek.pazola1998@gmail.com>
2023-12-12 01:46:23 +0100

    Use SwaggerUI in prod

--- LeagueOfStats.API/Program.cs
+++ LeagueOfStats.API/Program.cs
@@ -40,11 +40,8 @@ builder.Services.AddDomainDI(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
